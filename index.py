91666d56569dd95ee880044bcbf96e39c582d5ad
Loukas K <lukekonsta@gmail.com>
2024-03-02 13:37:18 +0200

    open-meteo data
    Receiving current weather data from open-meteo.

--- index.py
+++ index.py
@@ -1,4 +1,44 @@
-import datetime
+import openmeteo_requests
 
-x = datetime.datetime.now()
-print(x)
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 34.6841,
+	"longitude": 33.0379,
+	"current": ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "precipitation", "rain"],
+	"timezone": "auto",
+	"forecast_days": 1
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Current values. The order of variables needs to be the same as requested.
+current = response.Current()
+current_temperature_2m = current.Variables(0).Value()
+current_relative_humidity_2m = current.Variables(1).Value()
+current_apparent_temperature = current.Variables(2).Value()
+current_precipitation = current.Variables(3).Value()
+current_rain = current.Variables(4).Value()
+
+print(f"Current time {current.Time()}")
+print(f"Current temperature_2m {current_temperature_2m}")
+print(f"Current relative_humidity_2m {current_relative_humidity_2m}")
+print(f"Current apparent_temperature {current_apparent_temperature}")
+print(f"Current precipitation {current_precipitation}")
+print(f"Current rain {current_rain}")
